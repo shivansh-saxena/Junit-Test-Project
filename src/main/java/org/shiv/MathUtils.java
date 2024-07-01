07db907f49ceeaa156e5f09d959e0b12b7bf77ac
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-07-01 23:58:47 +0530

    @Test -> marks function as a test method Idea/ instead of using main we can use junit as a runner
    MathUtils.java is a simple class having a two integer add method
    MathUtilsTest.java is test class for MathUtils.java and has an add method with @Test annotation
    assertEquals is used for checking if output is as expected or not

---
+++ src/main/java/org/shiv/MathUtils.java
@@ -0,0 +1,7 @@
+package org.shiv;
+
+public class MathUtils {
+    public int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ src/test/java/org/shiv/MathUtilsTest.java
@@ -0,0 +1,17 @@
+package org.shiv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void add() {
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1,1);
+
+        assertEquals(expected,actual);
+    }
+}

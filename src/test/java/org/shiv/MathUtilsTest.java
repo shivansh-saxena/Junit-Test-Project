bbb68ded25e0f193b09f648c64802d42a7ed6e25
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-07-02 00:11:19 +0530

    Different assert-> 1. assertEquals(expected, actual) 2. assertArrayEquals(expected arrays, actual arrays) 3. assertIterableEquals(expected arrays, actual arrays) etc.. All methods can be found in Assertions java doc
    Test Driven Development -> write test cases first then do development

--- src/test/java/org/shiv/MathUtilsTest.java
+++ src/test/java/org/shiv/MathUtilsTest.java
@@ -7,11 +7,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class MathUtilsTest {
 
     @Test
-    void add() {
+    void testAdd() {
         MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1,1);
 
-        assertEquals(expected,actual);
+        assertEquals(expected,actual,"this is the message which will be returned(returned in case of failure)!!");
+    }
+
+    @Test
+    void testComputeCircleArea() {
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(Math.PI*10*10,mathUtils.computeCircleArea(10),"this method will return area of circle");
     }
 }
